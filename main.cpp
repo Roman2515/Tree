969cce25e0f2ae9aa1eecad4a7deb5e704c685d8
Roman <rmuliavin@mail.ru>
2016-04-10 10:59:04 +0600

    Second

--- main.cpp
+++ main.cpp
@@ -5,16 +5,34 @@ using namespace std;
 
 int main()
 {
+    cout << "Select options:\n\t1. Add node\n\t2. View tree\n\t3. Scearch\n";
+    int op;
     int in;
     string st;
     Tree<int, string> Od;
-    for(int i = 0; i < 5; i++)
-    {
+
+    while (true) {
+        cin >> op;
+    switch (op) {
+    case 1:
         cout << "set key : ";
         cin >> in;
         cout << "set data : ";
         cin >> st;
         Od.insertNode(in, st);
+        break;
+    case 2:
+        Od.preOrderTraversal();
+    case 3:
+        cin >> in;
+        if (Od.treeScearch(in, st))
+            cout << "key " << in << " is data: " << st << endl;
+        else
+            cout << "key not found\n";
+    default:
+            cout << "not found\n";
+        break;
     }
-    Od.inOrderTraversal();
+    }
+
 }
--- tree.h
+++ tree.h
@@ -10,9 +10,11 @@ class Tree
 public:
     Tree();
     void insertNode(const NODEKEY &, const NODEDATA &);
+    bool isEmpty();
     void preOrderTraversal() const;
     void inOrderTraversal() const;
     void postOrderTraversal()const;
+    bool treeScearch(const NODEKEY &value, NODEDATA &);
 private:
     TreeNode<NODEKEY, NODEDATA> *rootptr;
 
@@ -20,6 +22,7 @@ private:
     void preOrderHelper(TreeNode<NODEKEY, NODEDATA> *) const;
     void inOrderHelper(TreeNode<NODEKEY, NODEDATA> *) const;
     void postOrderHelper(TreeNode<NODEKEY, NODEDATA> *) const;
+    bool treeScearchHelper(TreeNode<NODEKEY, NODEDATA> *, const NODEKEY &, NODEDATA &);
 };
 
 template <class NODEKEY, class NODEDATA>
@@ -49,6 +52,32 @@ void Tree<NODEKEY, NODEDATA>::insertNodeHelper(TreeNode<NODEKEY, NODEDATA> **ptr
 }
 
 template <class NODEKEY, class NODEDATA>
+bool Tree<NODEKEY, NODEDATA>::isEmpty()
+{
+    if (rootptr == nullptr)
+        return false;
+    else
+        return true;
+}
+
+template <class NODEKEY, class NODEDATA>
+void Tree<NODEKEY, NODEDATA>::preOrderTraversal() const
+{
+    preOrderHelper( rootptr );
+}
+
+template <class NODEKEY, class NODEDATA>
+void Tree<NODEKEY, NODEDATA>::preOrderHelper(TreeNode<NODEKEY, NODEDATA> *ptr) const
+{
+    if (ptr != nullptr)
+    {
+        std::cout << ptr->key << " : " << ptr->data << "  |  ";
+        preOrderHelper(ptr->leftptr);
+        preOrderHelper(ptr->rightptr);
+    }
+}
+
+template <class NODEKEY, class NODEDATA>
 void Tree<NODEKEY, NODEDATA>::inOrderTraversal() const
 {
     inOrderHelper( rootptr );
@@ -59,10 +88,52 @@ void Tree<NODEKEY, NODEDATA>::inOrderHelper(TreeNode<NODEKEY, NODEDATA> *ptr) co
 {
     if (ptr != nullptr)
     {
-        inOrderHelper( ptr->leftptr);
+        inOrderHelper(ptr->leftptr);
         std::cout << ptr->key << " : " << ptr->data << "  |  ";
-        inOrderHelper( ptr->rightptr);
+        inOrderHelper(ptr->rightptr);
     }
+}
+
+template <class NODEKEY, class NODEDATA>
+void Tree<NODEKEY, NODEDATA>::postOrderTraversal() const
+{
+    postOrderHelper( rootptr );
+}
+
+template <class NODEKEY, class NODEDATA>
+void Tree<NODEKEY, NODEDATA>::postOrderHelper(TreeNode<NODEKEY, NODEDATA> *ptr) const
+{
+    if (ptr != nullptr)
+    {
+        postOrderHelper( ptr->leftptr);
+        postOrderHelper( ptr->rightptr);
+        std::cout << ptr->key << " : " << ptr->data << "  |  ";
+    }
+}
+
+template <class NODEKEY, class NODEDATA>
+bool Tree<NODEKEY, NODEDATA>::treeScearch(const NODEKEY &value, NODEDATA &val)
+{
+    if (isEmpty())
+    return treeScearchHelper(rootptr, value, val);
+    std::cout << "tree is empty";
+    return false;
+}
+
+template <class NODEKEY, class NODEDATA>
+bool Tree<NODEKEY, NODEDATA>::treeScearchHelper(TreeNode<NODEKEY, NODEDATA> *ptr, const NODEKEY &value, NODEDATA &val)
+{
+    if (ptr == nullptr)
+        return false;
+    if (value == ptr->key)
+    {
+         val = ptr->data;
+         return true;
+    }
+    if (value < ptr->key)
+        treeScearchHelper(ptr->leftptr, value, val);
+    else if (value > ptr->key)
+        treeScearchHelper(ptr->rightptr, value, val);
 }
 
 
